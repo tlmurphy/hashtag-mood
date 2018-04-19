9125ac04ee4a380091673ab664dd45725f4efc13
Trevor Murphy <murphtrevor@gmail.com>
2018-04-19 15:38:29 -0500

    Add example chart (#1)
    Also added an easier way to load in the Twitter keys.

--- app.py
+++ app.py
@@ -2,18 +2,30 @@ from flask import Flask, render_template, jsonify, request
 from twitter_mood.mood_gatherer import TwitterMoodGatherer
 import twitter
 import os
+from pathlib import Path
 
 app = Flask(__name__)
 
-consumer_key = os.environ['CON_KEY']
-consumer_secret = os.environ['CON_SECRET']
-access_token_key = os.environ['ACC_KEY']
-access_token_secret = os.environ['ACC_SECRET']
+my_file = Path("./twitter-config")
+try:
+    my_abs_path = my_file.resolve()
+except FileNotFoundError:
+    consumer_key = os.environ['CON_KEY']
+    consumer_secret = os.environ['CON_SECRET']
+    access_token_key = os.environ['ACC_KEY']
+    access_token_secret = os.environ['ACC_SECRET']
+else:
+    config = {}
+    exec(open("./.twitter-config").read(), config)
+    consumer_key = config['consumer_key']
+    consumer_secret = config['consumer_secret']
+    access_token_key = config['access_key']
+    access_token_secret = config['access_secret']
+
 twitter_api = twitter.Api(consumer_secret=consumer_secret,
                           access_token_key=access_token_key,
                           access_token_secret=access_token_secret,
                           consumer_key=consumer_key)
-
 
 @app.route('/')
 def hello_world():
